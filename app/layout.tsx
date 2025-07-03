c1d35fccc6bceb3b7aa9e1a5526aafe9bc3165bc
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-07-03 16:02:50 +0300

    layout metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,27 @@ export const metadata: Metadata = {
   title: "NoteHub",
   description:
     "A simple and efficient application for managing personal notes.",
+  openGraph: {
+    title: "NoteHub App",
+    description: "A simple application for managing personal notes.",
+    url: "https://08-zustand-psi.vercel.app/",
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub App",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NoteHub App",
+    description: "A simple application for managing personal notes.",
+    images: ["https://ac.goit.global/fullstack/react/notehub-og-meta.jpg"],
+  },
 };
 
 export default function RootLayout({
